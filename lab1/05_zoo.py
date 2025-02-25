4d47e7bddf81884e5e55a900bb862419594f1e90
Strelets Vladislav <streletsvlad@yandex.ru>
2025-02-25 11:18:55 +0500

    1 лабораторная

--- lab1/05_zoo.py
+++ lab1/05_zoo.py
@@ -9,15 +9,29 @@ zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
 #  и выведите список на консоль
 # TODO здесь ваш код
 
+zoo.insert(1, 'bear')
+print(zoo)
+
 # добавьте птиц из списка birds в последние клетки зоопарка
 birds = ['rooster', 'ostrich', 'lark', ]
 #  и выведите список на консоль
 # TODO здесь ваш код
 
+zoo.extend(birds)
+print(zoo)
+
 # уберите слона
 #  и выведите список на консоль
 # TODO здесь ваш код
 
+zoo.remove('elephant')
+print(zoo)
+
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
 # TODO здесь ваш код
+
+lion = zoo.index('lion') + 1
+lark = zoo.index('lark') + 1
+print('лев сидит в клетке ', lion)
+print('жаворонок сидит в клетке ', lark)
